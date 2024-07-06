faedfd6c2c08b50a513f0eed730089f5027fcc9e
Boqiang Liang <boqiangliang@Boqiangs-MacBook-Pro.local>
2024-07-06 09:37:01 -0700

    added streamlit

---
+++ venv/streamlitapp.py
@@ -0,0 +1,18 @@
+# python3 -m pip install streamlit
+
+# pip install streamlit
+
+import streamlit as st
+import pandas as pd
+
+
+st.write("""
+# My first app
+Hello *world!*
+""")
+
+# streamlit run
+
+
+
+
